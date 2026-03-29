2d099fa3ce6da74b120711cf5e3a81497aabccdb
Tharun Teja <pellakurutharunteja@gmail.com>
2026-03-29 23:25:05 +0530

    Client Edit

--- controllers/clientsController.js
+++ controllers/clientsController.js
@@ -96,7 +96,7 @@ const updateClient = (req, res) => {
     client_country: { required: true },
     client_state: { required: true },
     client_currency: { required: true },
-    client_status: { required: true, enum: ['Active', 'Inactive', 'On Hold', 'Completed', 'Dropped'] }
+    client_status: { required: true, enum: ['Active', 'Inactive'] }
   });
 
   if (error) {
