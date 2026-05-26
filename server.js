d16038e073cb6f3c643ef557628b58e0037af19f
Tharun Teja <pellakurutharunteja@gmail.com>
2026-05-26 11:43:29 +0530

    Enquiry Chnages

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ const app = express();
 
 // Middlewares
 const corsOptions = {
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"]
 };
